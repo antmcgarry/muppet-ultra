f62c2d354809077f3ff928ceb28d4deb0ee5d339
Anthony Mcgarry <anthony.mcgarry@wearenova.co.uk>
2018-09-09 20:50:41 +0100

    Created a Test Scene with a pistol and AK-47 working

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+
+    public Animator animator;
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (isReloading) return;
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+		
+	}
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        animator.SetBool("Reloading", true);
+        Debug.Log("Reloading....");
+        yield return new WaitForSeconds(reloadTime - .25f);
+        currentAmmo = maxAmmo;
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+        isReloading = false;
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        currentAmmo--;
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if( target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject imapctObject = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            Destroy(imapctObject, 2f);
+        }
+    }
+}
